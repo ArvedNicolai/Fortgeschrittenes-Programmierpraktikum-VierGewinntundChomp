cc6a2a1b50a5d438b39e77a38d847464d709975a
ArvedArv <arvdt@gmx.de>
2019-10-22 21:53:28 +0200

    Neuer Test
    Schon wieder

---
+++ viergewinntundchomp/src/viergewinntundchomp/main.java
@@ -0,0 +1,12 @@
+package viergewinntundchomp;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 2, b = 4;
+		int c = a + b;
+		System.out.print(c);
+	}
+
+}
